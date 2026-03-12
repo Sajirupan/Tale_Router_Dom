d3d9bc38aa5d422a8f0b8a92d82b5af5c46126fe
Sajirupan <balabavansajirupan@gmail.com>
2026-03-12 15:21:08 +0530

    error work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import './App.css';
-import './css/animate.css';
-import './css/fontawesome.css';
-import './css/templatemo-tale-seo-agency.css';
-import './css/owl.css';
-import './css/flex-slider.css';
-import Navbar from './components/navbar';
-import { Routes, Route} from 'react-router-dom';
-import Home from './pages/home';
-import Aboutus from './pages/about';
-import Faq from './pages/faq';
-import Contact from './pages/contact';
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Home from "./pages/Home";
+import About from "./pages/AboutUs";
+import Faq from "./pages/Faq";
+import Contact from "./pages/Contact";
+import "./assets/css/animate.css";
+import "./assets/css/flex-slider.css";
+import "./assets/css/fontawesome.css";
+import "./assets/css/owl.css";
+import "./assets/css/templatemo-tale-seo-agency.css";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-     <Navbar/>
-     <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<Aboutus/>}/>
-        <Route path='/faq' element={<Faq/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/TaleRouterDom"  element={<Home/>} />
+        <Route path="/faqs"  element={<Faq/>} />
+        <Route path="/about_us"  element={<About/>} />
+        <Route path="/contact"  element={<Contact/>} />
       </Routes>
-    </div>
+    </>
   );
 }
+
 export default App;
